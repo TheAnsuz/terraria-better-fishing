d61c98f13aa42a91c77a9ea1c0ca45b6fd056401
Ansuz <adrianmrv02@gmail.com>
2023-10-16 14:17:34 +0200

    Published 2.1.0

--- Hooks/ModPlayerHooks.cs
+++ Hooks/ModPlayerHooks.cs
@@ -8,23 +8,6 @@ namespace BetterFishing.Hooks
 {
     internal class ModPlayerHooks : ModPlayer
     {
-        internal float AumentedFishingPowerAmount = 0;
-
-        public override void ResetEffects()
-        {
-            base.ResetEffects();
-            AumentedFishingPowerAmount = 0;
-        }
-
-        internal static PlayerFishingConditions CustomFishingConditions(On_Player.orig_GetFishingConditions orig, Player self)
-        {
-            PlayerFishingConditions conds = orig(self);
-
-            conds.FinalFishingLevel += (int)self.GetModPlayer<ModPlayerHooks>().AumentedFishingPowerAmount;
-
-            return conds;
-        }
-
         public override void OnEnterWorld()
         {
             base.OnEnterWorld();
@@ -32,11 +15,6 @@ namespace BetterFishing.Hooks
             if (BetterFishing.Errors)
                 Main.NewText("BetterFishing loaded with errors", Color.Red);
 #endif
-        }
-
-        public override void GetFishingLevel(Item fishingRod, Item bait, ref float fishingLevel)
-        {
-            //fishingLevel += AumentedFishingPowerAmount;
         }
     }
 }
