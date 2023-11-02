1c4c66dafd84f6e29c3affc0bab262310ee4aecb
Ansuz <adrianmrv02@gmail.com>
2023-11-02 13:40:50 +0100

    Added custom sell conditions

---
+++ AnglerShop/SellCondition/CustomSellCondition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace BetterFishing.AnglerShop.SellCondition
+{
+    public class CustomSellCondition : SellCondition
+    {
+        private readonly CustomCheck _check;
+
+        internal CustomSellCondition(CustomCheck check)
+        {
+            _check = check;
+        }
+
+        public override bool Acomplishes(NPC npc, Player player, Item[] items)
+        {
+            return _check(npc, player, items);
+        }
+
+        public delegate bool CustomCheck(NPC npc, Player player, Item[] items);
+    }
+}
