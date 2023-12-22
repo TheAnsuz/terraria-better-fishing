153a019795a3bb246bec0537d586277a85e9e5a7
Ansuz <adrianmrv02@gmail.com>
2023-12-22 23:15:38 +0100

    code save

--- AnglerShop/ReloadShopForPlayer.cs
+++ AnglerShop/ReloadShopForPlayer.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.Chat;
 using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace BetterFishing.AnglerShop
@@ -16,6 +19,25 @@ namespace BetterFishing.AnglerShop
         {
             if (AnglerShop.IsOpen)
                 AnglerShop.OpenAnglerShop();
+        }
+
+        public override bool CanBuyItem(NPC vendor, Item[] shopInventory, Item item)
+        {
+            // See branch_gimmic
+            if (vendor.type != NPCID.Angler)
+                return base.CanBuyItem(vendor, shopInventory, item);
+
+            if (!AnglerShop.IsOpen)
+                return base.CanBuyItem(vendor, shopInventory, item);
+
+            AnglerShopEntry entry = AnglerShop.GetEntry(item.type);
+
+            if (entry == null)
+                return base.CanBuyItem(vendor, shopInventory, item);
+
+            ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(entry.ToString()), Color.White);
+
+            return true;
         }
     }
 }
